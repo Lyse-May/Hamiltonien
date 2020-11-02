24bfa64a69879f130ed4daf34c846bd8431d7764
Capucine <Capucine@LAPTOP-1B04RBRH>
2020-11-02 22:03:19 +0100

    Update TP1_AFKIR_CANNET_HOAREAU.py

--- TP1/TP1_AFKIR_CANNET_HOAREAU.py
+++ TP1/TP1_AFKIR_CANNET_HOAREAU.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 31 16:30:54 2020
+Created on Sat Oct 31 17:46:31 2020
 
-@author: Capucine
+@author: HOAREAU.LyseMay
 """
 import numpy as np
 from matplotlib import pyplot as plt
@@ -16,21 +17,101 @@ of the interest at time t and reinvests the rest. What should the investor do to
 consumption before T ?
 
 """
-
-
-
-"Question 1) Implement the bang-bang controller described in class using the programming language Python"
+# Definition of variables
 
 r = 0.05
 T = 50
 
-a = np.random.rand(T, None,'F')
-X = np.ones(T,None,'F')
+X = np.zeros((T,1))
+X[0] = 1000000
+
+A = np.arange(0,1,1/(T-1))
+
+W = np.zeros((T,1))
+rho = np.zeros((T,1))
+rho[-1] = 1 
+time = np.array([i for i in range(0,T)])
 
 
-for i in range (1,T):
-    X[i] += r*X[i-1]*(1-a)
+"Question 1) Implement the bang-bang controller described in class using the programming language Python"
+
+# Plant equation    
     
+def plant(A): 
+    for i in range (1,T):
+        X[i] = X[i-1] + r*X[i-1]*(1-A[i-1])
+    return X
 
-for i in range (0,T):
-    W= r*X[i]*a[i]
+# Definition of rho, a, X and W by Bellman method
+    
+def Bellman():
+    for i in range (T,0,-1):
+        
+        if r >= 1/rho[i-1]:
+            rho[i-2]=(1+r) * rho[i-1]
+            A[i-2] = 0
+        if r < 1/rho[i-1]:
+            rho[i-2] = 1 + rho[i-1]
+            A[i-2] = 1
+        rho[-1] = 1 
+        
+    X = plant(A)
+    for i in range (0,T):
+        W[i] = r * X[i] * rho[i]
+    return rho,A,X,W
+
+
+rho,A,X,W = Bellman() 
+A[-1] = 1   
+
+
+# Display of results
+
+Y1 = (1 + rho)
+Y2 = (1 + r )* rho
+
+
+plt.plot(rho,Y1,label = 'Y1')
+plt.plot(rho,Y2,label = 'Y2')
+plt.ylabel("y")
+plt.xlabel("rho")
+plt.legend()
+plt.xlim(10,40)
+plt.ylim(10,40)
+plt.show()
+
+
+"Question 2) Compute the corresponding total consumption and find the sequence of optimal actions."
+
+def consumption(A):
+    X_cons = np.zeros((T,1))
+    S=0
+    for i in range (1,T+1):
+        X_cons[i-1] = r*X[i-1]*A[i-1]
+        S += X_cons[i-1]
+    return X_cons,S
+
+X_cons,S = consumption(A)
+print("Total Consumption : ",S)
+
+
+"Question 3) Plot the consumption as a function of time."
+
+plt.plot(time,X_cons)
+plt.ylabel("Consommation")
+plt.xlabel("Time")
+plt.show()
+
+
+
+"Question 4) Plot the action sequence as a function of time."
+
+plt.plot(time,A)
+plt.ylabel("Action")
+plt.xlabel("Time")
+plt.show()
+
+
+"Question 5) Choose a couple of other strategies (controllers) to compare their respective total consumption to that obtained using the bang-bang approach."
+
+
