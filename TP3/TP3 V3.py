6071c7c01a8da9c9523220bba2f08cb611c9fd07
Lyse-May <59876881+Lyse-May@users.noreply.github.com>
2020-11-18 15:55:49 +0100

    Update TP3 V3.py

--- TP3/TP3 V3.py
+++ TP3/TP3 V3.py
@@ -61,7 +61,7 @@ def state(p,Nb,Cash):
     S = [Price,Nb,Cash]
     return S
 
-#S = state(close_train,Nb,Cash_init) 
+#S = state(close_train,Nb_init,Cash_init) 
 #print(S) 
 
 def reward(S,a,t):
@@ -69,19 +69,24 @@ def reward(S,a,t):
     Cash = S[2]
     Nb = S[1]
     S = S[0]
-    if (S[t][-1] >= (np.mean(S[t]) * 0.99)):
-        if (a == 1) & (Nb >= 30): #vendre
+    if (a == 1) & (Nb >= 30):
+        if (S[t][-1] >= (np.mean(S[t]) * 0.99)): #vendre
             R = S[t][-1] * Nb_share
             Cash+= R
             Nb -= Nb_share
-    if (S[t][-1] <= (np.mean(S[t]) * 1.01)):
-        if (a == 2) & (Cash >= S[t][-1]*Nb_share): #achat
-            R = -S[t][-1] * Nb_share
-            Cash+= R
-            Nb += Nb_share
+    if (a == 2): 
+        if (Cash >= S[t][-1] * Nb_share):
+            if (S[t][-1] <= (np.mean(S[t]) * 1.01)): #achat
+                R = -S[t][-1] * Nb_share
+                Cash+= R
+                Nb += Nb_share
+    if (a == 0):
+        R = 0
+        Nb = Nb
+        Cash = Cash
     return R,Nb,Cash
 
-#R = reward(S,1,0)
+#R = reward(state(close_train,90,2000),2,15)
 #print(R)
 
 def portfolio(ptf_prec,S,a,t):
@@ -93,13 +98,12 @@ def portfolio(ptf_prec,S,a,t):
         ptf += Cash
     return ptf
 
-alpha = 0.3
-gamma = 0.1
-
-
 """
 QUESTION 4
 """
+
+alpha = 0.3
+gamma = 0.1
 
 def maxi(cash_prec,S,t):
     A = np.zeros((3,1))
@@ -109,39 +113,52 @@ def maxi(cash_prec,S,t):
     a = index[0][0] 
     return A[a],a
 
-def Q_learning(data):
-    Q = np.zeros((n,3))
-    S = state(data,Nb_init,Cash_init)
-    for t in range(0,n-1):
-        for a in range(0,3):
-            R,Nb,Cash = reward(S,a,t)
-            Qmax = maxi(Cash,S,t+1)
-            for itera in range(0,20):
-                Q[t,a] = (1 - alpha) * Q[t,a] + alpha * (R + gamma * Qmax[0])                
-        R,Nb,Cash = reward(S,Qmax[1],t)
-    return Q
-
-Q = Q_learning(close_train)
-PI = np.argmax(Q)
-print(Q)
-print(PI)
-
-
-#def Q_learning(S):
-#    Q = np.zeros((n,1))
-#    res,Nb,_ = reward(0,1,S,0,0)
-#    for t in range(1,n):
+#def Q_learning(data):
+#    Q = np.zeros((n-1,3))
+#    S = state(data,Nb_init,Cash_init)
+#    for t in range(0,n-1):
+#        for a in range(0,3):
+#            R,Nb,Cash = reward(S,a,t)
+#            Qmax = maxi(Cash,S,t+1)
+#            for itera in range(0,20):
+#                Q[t,a] = (1 - alpha) * Q[t,a] + alpha * (R + gamma * Qmax[0])                
 #        
-#        for itera in range(0,10):
-#            
-#            a = maxi(res,S,t-1,Nb)
-#            at = np.random.choice([-1,0,1])
-#            Q[t-1],_,R = reward(res,at,S,t-1,Nb)
-#            Q[t-1] = (1 - alpha) * Q[t-1] + alpha * (R + gamma * reward(Q[t-1],a,S,t,Nb)[0])
-#        
-#        res,Nb,_ = reward(Q[t-1],a,S,t-1,Nb) 
-#    
+#        S = state(data,Nb,Cash)
+#        _,Nb,Cash = reward(S,Qmax[1],t)
 #    return Q
+#
+#Q = Q_learning(close_train)
+#print(Q)
+
+def Q_learning(data):
+    Q = np.zeros((n-1,3))
+    #S = state(data,Nb_init,Cash_init)
+    Nb_prec,Cash_prec = Nb_init,Cash_init
+    Cashs = [Cash_prec]
+    for t in range(1,n):
+        S = state(data,Nb_prec,Cash_prec)
+        for a in range(0,3):
+            
+            R,Nb,Cash = reward(S,a,t-1)
+            Qmax = maxi(Cash,S,t)
+            
+            for itera in range(0,100):
+                Q[t-1,a] = (1 - alpha) * Q[t-1,a] + alpha * (R + gamma * Qmax[0])                
+            
+        R,Nb_new,Cash_new = reward(state(data,Nb,Cash),Qmax[1],t)
+        Nb_prec,Cash_prec = Nb_new,Cash_new
+        
+        Cashs.append(Cash_new)
+    return Q,Cashs
+
+Q,Cash = Q_learning(close_train)
+
+plt.plot(date_train[1:],Q[:,0], label = 'A = 0')
+plt.plot(date_train[1:],Q[:,1], label = 'A = 1')
+plt.plot(date_train[1:],Q[:,2], label = 'A = 2')
+plt.legend()
+plt.show()
+plt.plot(date_train,Cash, label = 'Cash')
 
 
 
